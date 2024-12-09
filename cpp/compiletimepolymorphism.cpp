caeb53eb84470564a725ce31dd8d58c29b5b0f56
Siddhant Jadhav <siddhantjadhav0106@gmail.com>
2024-12-09 12:21:58 +0530

    compile time polymorphism

---
+++ cpp/compiletimepolymorphism.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+class poly{
+            public:
+            void func(int x)
+            {
+                        cout << "Value of x is " << x << endl;
+            }
+            void func(double x)
+            {
+                        cout << "Value of x is " << x << endl;
+            }
+            void func(int x , int y)
+            {
+                        cout << "Value of x and y is " << x << ',' << y << endl;
+            }
+};
+int main() 
+{
+            poly obj1;
+            obj1.func(7);
+            obj1.func(9.132);
+            obj1.func(85, 64);
+  return 0;
+}
