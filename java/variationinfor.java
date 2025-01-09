b4781fd03a8731421ad3bb7f356593532768c4ec
Siddhant Jadhav <siddhantjadhav0106@gmail.com>
2025-01-09 11:07:31 +0530

    Create variationinfor.java

---
+++ java/variationinfor.java
@@ -0,0 +1,8 @@
+public class variationfor {
+            public static void main(String[] args) {
+                for (int a = 1, b = 6; a < b; a++, b--) {
+                    System.out.println(a + " " + b);
+                }
+            }
+        }
+   
